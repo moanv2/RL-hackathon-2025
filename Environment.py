e9937ad9c1ffa6dfebf478662edf5097a3a80e72
Mehdi Zaid <mehdizaid02@gmail.com>
2025-04-03 12:44:19 +0000

    Run experiment 1

--- Environment.py
+++ Environment.py
@@ -440,10 +440,17 @@ class Env:
         """
         Calculate reward for a given step of the environment and for a given bot
 
-        Reward vars
-        R = w₁S + w₂D + w₃K + w₄H + w₅E + w₆A
         """
         reward = 0
+
+        health_penalty_weight = 0.1
+        damage_reward_weight = 0.1
+        kill_reward_weight = 100
+        movement_reward_weight = 0.01
+        rotation_penalty_weight = 0.01
+        ammo_penalty_weight = 0.01
+        approach_reward_weight = 0.1
+        death_penalty = 100
 
         players_info = info_dictionary.get("players_info", {})
         player_current_info = players_info.get(bot_username)
@@ -452,64 +459,40 @@ class Env:
         assert player_current_info is not None
         assert player_previous_info is not None
 
-        # --- Rewarding closing distance
-        current_distance_to_opponent = self._get_distance_to_opponent(player_current_info)
-        previous_distance_to_opponent = self._get_distance_to_opponent(player_previous_info)
+        # Penalize health loss
+        health_diff = player_current_info["health"] - player_previous_info["health"]
+        if health_diff < 0:
+            reward += health_diff * health_penalty_weight
+        
+        # Reward hitting enemy
+        damage_diff = player_current_info["damage_dealt"] - player_previous_info["damage_dealt"]
+        reward += damage_diff * damage_reward_weight
 
-        if current_distance_to_opponent < 20:
-            reward += (20 - current_distance_to_opponent) * 0.3
+        # Reward kills
+        kill_diff = player_current_info["kills"] - player_previous_info["kills"]
+        reward += kill_diff * kill_reward_weight
 
-        # --- Encourage retreating / penalize not retreating when taking damage
-        current_health = player_current_info.get("health")
-        previous_health = player_previous_info.get("health")
+        # Reward exploration (movement)
+        distance_moved = player_current_info["meters_moved"] - player_previous_info["meters_moved"]
+        reward += distance_moved * movement_reward_weight
 
-        if current_health < previous_health:
-            health_diff = previous_health - current_health
-            # cap diff to prevent HUGE spikes (due to two unsuccessful runs)
-            health_diff = min(health_diff, 20)
+        # Penalize excessive spinning
+        rotation_diff = player_current_info["total_rotation"] - player_previous_info["total_rotation"]
+        reward -= abs(rotation_diff) * rotation_penalty_weight
 
-            if current_distance_to_opponent <= previous_distance_to_opponent:
-                reward -= (previous_distance_to_opponent - current_distance_to_opponent) * 0.2
-            else:  # Reward Bot for retreating
-                reward += (current_distance_to_opponent - previous_distance_to_opponent) * 0.1
+        # Penalize ammo wasting
+        ammo_diff = player_current_info["current_ammo"] - player_previous_info["current_ammo"]
+        if ammo_diff < 0:
+            reward += ammo_diff * ammo_penalty_weight
 
-        # -----------------------------
-        # Scanning opponents with ray (Variable 2: Accuracy/Precision)
-        # Goal: Encourage agent to be more precise and have better accuracy since it always shoots from the middle ray
+        # Reward getting closer to enemy
+        prev_dist = self._get_distance_to_opponent(player_previous_info)
+        curr_dist = self._get_distance_to_opponent(player_current_info)
+        dist_diff = prev_dist - curr_dist
+        reward += dist_diff * approach_reward_weight
 
-        accuracy_metrics = self.accuracy_precision(player_current_info)
-        # Fixed variable name to match accuracy_precision function
-        is_targeting_player = accuracy_metrics["is_targeting_player"]
-        shot_accuracy = accuracy_metrics["shot_accuracy"]
-
-        shot_fired = player_current_info.get("shot_fired")
-
-        reward += shot_accuracy * 0.2
-
-        if is_targeting_player and shot_fired:
-            current_damage = player_current_info.get("damage_dealt", 0)
-            previous_damage = player_previous_info.get("damage_dealt", 0)
-
-            if current_damage > previous_damage:
-                damage_diff = current_damage - previous_damage
-                # same as above capping limit
-                damage_diff = min(damage_diff, 20)
-                reward += damage_diff * 0.2  # reward for hitting
-
-        # Exploration
-        exploration_metrics = self.bot_exploration(player_current_info)
-        exploration_score = exploration_metrics["exploration_score"]
-        reward += exploration_score * 0.15
-
-        # Movement efficiency
-        efficiency_metrics = self.calculate_movement_efficiency(player_current_info)
-        movement_efficiency = efficiency_metrics["normalized_efficiency"]
-        reward += movement_efficiency * 0.12
-
-        # Track and get learning rate metrics
-        learning_metrics = self.track_learning_progress(bot_username, reward)
-        learning_rate_score = learning_metrics["learning_rate_score"]
-        reward += learning_rate_score * 0.1
+        # Penalize death
+        if not player_current_info["alive"] and player_previous_info["alive"]:
+            reward -= death_penalty
 
         return reward
-
